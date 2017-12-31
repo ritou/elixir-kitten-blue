3e1ed175aecf110ee9c201b01acf4057064d0c3f
ritou <ritou.06@gmail.com>
2018-01-01 01:39:34 +0900

    add list_to_public_jwk_sets function

--- lib/kitten_blue/jwk.ex
+++ lib/kitten_blue/jwk.ex
@@ -11,7 +11,41 @@ defmodule KittenBlue.JWK do
   
   @type t :: %__MODULE__{kid: String.t, alg: String.t, key: JOSE.JWK.t}
 
+  @doc """
+  ```
+  kb_jwk = KittenBlue.JWK.new([kid: kid, alg: alg, key: key])
+  ```
+  """
+  @spec new(opts :: Keywords.t) :: t
   def new(opts) do
     struct(__MODULE__, Map.new(opts))
+  end
+
+  @doc """
+  Convert to `JSON Web Key Sets` format public keys from `KittenBlue.JWK` list.
+
+  ```
+  public_jwk_sets = KittenBlue.JWK.list_to_public_jwk_sets(jwk_list)
+  ```
+  """
+  @spec list_to_public_jwk_sets(jwk_list :: List.t) :: map
+  def list_to_public_jwk_sets(jwk_list) when is_list(jwk_list) do
+    %{"keys" =>
+      jwk_list
+      |> Enum.map(fn(jwk) -> to_public_key_map(jwk) end)
+      |> Enum.filter(& !is_nil(&1))
+    }
+  end
+
+  defp to_public_key_map(jwk = %__MODULE__{}) do
+    jwk.key
+    |> JOSE.JWK.to_public()
+    |> JOSE.JWK.to_map()
+    |> elem(1)
+    |> Map.put("alg", jwk.alg)
+    |> Map.put("kid", jwk.kid)
+  end
+  defp to_public_key_map(_) do
+    nil
   end
 end

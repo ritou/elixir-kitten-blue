501c8cfd5756efd1aa6465bd08dd9d4169c47e94
ritou <ritou.06@gmail.com>
2018-01-05 01:55:47 +0900

    add ex_docs to dev deps

--- mix.exs
+++ mix.exs
@@ -24,7 +24,10 @@ defmodule KittenBlue.Mixfile do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       {:jose, "~> 1.8.4"},
-      {:poison, "~> 3.1.0"}
+      {:poison, "~> 3.1.0"},
+
+      # for docs
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false},
     ]
   end
 end

16b42fec4674164aa39e0f38e19a054c97199239
ritou <ritou.06@gmail.com>
2018-04-08 03:33:27 +0900

    run formatter

--- lib/kitten_blue/jwk.ex
+++ lib/kitten_blue/jwk.ex
@@ -9,7 +9,7 @@ defmodule KittenBlue.JWK do
     :key
   ]
 
-  @type t :: %__MODULE__{kid: String.t, alg: String.t, key: JOSE.JWK.t}
+  @type t :: %__MODULE__{kid: String.t(), alg: String.t(), key: JOSE.JWK.t()}
 
   @doc """
   ```Elixir
@@ -20,7 +20,7 @@ defmodule KittenBlue.JWK do
   kb_jwk = KittenBlue.JWK.new([kid: kid, alg: alg, key: key])
   ```
   """
-  @spec new(opts :: Keywords.t) :: t
+  @spec new(opts :: Keywords.t()) :: t
   def new(opts) do
     struct(__MODULE__, Map.new(opts))
   end
@@ -33,12 +33,13 @@ defmodule KittenBlue.JWK do
   public_jwk_sets = KittenBlue.JWK.list_to_public_jwk_sets(kb_jwk_list)
   ```
   """
-  @spec list_to_public_jwk_sets(jwk_list :: List.t) :: map
+  @spec list_to_public_jwk_sets(jwk_list :: List.t()) :: map
   def list_to_public_jwk_sets(jwk_list) when is_list(jwk_list) do
-    %{"keys" =>
-      jwk_list
-      |> Enum.map(fn(jwk) -> to_public_jwk_set(jwk) end)
-      |> Enum.filter(& !is_nil(&1))
+    %{
+      "keys" =>
+        jwk_list
+        |> Enum.map(fn jwk -> to_public_jwk_set(jwk) end)
+        |> Enum.filter(&(!is_nil(&1)))
     }
   end
 
@@ -58,6 +59,7 @@ defmodule KittenBlue.JWK do
     |> Map.put("alg", jwk.alg)
     |> Map.put("kid", jwk.kid)
   end
+
   def to_public_jwk_set(_) do
     nil
   end
@@ -69,12 +71,13 @@ defmodule KittenBlue.JWK do
   kb_jwk_list = KittenBlue.JWK.public_jwk_sets_to_list(public_jwk_sets)
   ```
   """
-  @spec public_jwk_sets_to_list(public_json_web_key_sets :: map) :: List.t
+  @spec public_jwk_sets_to_list(public_json_web_key_sets :: map) :: List.t()
   def public_jwk_sets_to_list(_ = %{"keys" => public_jwk_sets}) when is_list(public_jwk_sets) do
     public_jwk_sets
-    |> Enum.map(fn(public_jwk_set) -> from_public_jwk_set(public_jwk_set) end)
-    |> Enum.filter(& !is_nil(&1))
+    |> Enum.map(fn public_jwk_set -> from_public_jwk_set(public_jwk_set) end)
+    |> Enum.filter(&(!is_nil(&1)))
   end
+
   def public_jwk_sets_to_list(_) do
     []
   end
@@ -92,7 +95,7 @@ defmodule KittenBlue.JWK do
       with alg when alg != nil <- jwk_map["alg"],
            kid when kid != nil <- jwk_map["kid"],
            key = %JOSE.JWK{} <- jwk_map |> JOSE.JWK.from_map() do
-        new([kid: kid, alg: alg, key: key])
+        new(kid: kid, alg: alg, key: key)
       else
         _ -> nil
       end
@@ -100,8 +103,8 @@ defmodule KittenBlue.JWK do
       _ -> nil
     end
   end
+
   def from_public_jwk_set(_) do
     nil
   end
-
 end

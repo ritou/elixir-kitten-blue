4dd724c83f04859f1427affafae62b49f43872f3
ritou <ritou.06@gmail.com>
2018-07-08 02:01:29 +0900

    update deps

--- mix.exs
+++ mix.exs
@@ -31,6 +31,10 @@ defmodule KittenBlue.Mixfile do
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       {:jose, "~> 1.8.4"},
       {:poison, "~> 3.1.0"},
+      {:httpoison, "~> 1.2.0"},
+
+      # for test
+      {:mock, "~> 0.3.0", only: :test},
 
       # for docs
       {:ex_doc, "~> 0.16", only: :dev, runtime: false},

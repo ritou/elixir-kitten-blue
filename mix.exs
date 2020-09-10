7618443f730deb257e365ecf3db5138323a3998c
Kodai Hashimoto <kodai.hashimoto@mixi.co.jp>
2020-09-10 11:05:55 +0900

    relax dependency constraints and set `optional: true` for libs for jwks_uri

--- mix.exs
+++ mix.exs
@@ -34,9 +34,9 @@ defmodule KittenBlue.Mixfile do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:jose, "~> 1.8.4"},
-      {:poison, "~> 3.1.0"},
-      {:httpoison, "~> 1.4"},
+      {:jose, "~> 1.8"},
+      {:jason, "~> 1.2", optional: true},
+      {:httpoison, "~> 1.4", optional: true},
 
       # for test
       {:mox, "~> 0.5", only: :test},

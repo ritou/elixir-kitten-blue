5f175bbbc461af570967eb4320e84f5ce76492bc
ritou <ritou.06@gmail.com>
2020-09-14 02:49:27 +0900

    add scratcher to deps

--- mix.exs
+++ mix.exs
@@ -32,14 +32,12 @@ defmodule KittenBlue.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       {:jose, "~> 1.10"},
       {:jason, "~> 1.2", optional: true},
-      {:httpoison, "~> 1.4", optional: true},
 
       # for test
       {:mox, "~> 0.5", only: :test},
+      {:scratcher, "~> 0.1", only: :test},
 
       # for docs
       {:ex_doc, "~> 0.21.3", only: :dev, runtime: false}

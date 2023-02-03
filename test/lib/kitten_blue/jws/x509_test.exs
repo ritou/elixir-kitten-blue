8ae6ed36a9444f4adaf86ae22623d7455958154e
k-asm <asami.kosuke@gmail.com>
2023-02-03 13:36:54 +0900

    test: add tests to sign by JWK that includes x509 field

--- test/lib/kitten_blue/jws/x509_test.exs
+++ test/lib/kitten_blue/jws/x509_test.exs
@@ -3,6 +3,45 @@ defmodule KittenBlue.JWS.X509Test do
 
   alias KittenBlue.{JWK, JWS}
 
+  test "sign" do
+    key = File.read!("sample_pem/rsa-2048.pem") |> X509.PrivateKey.from_pem!()
+
+    root_ca =
+      X509.Certificate.self_signed(
+        key,
+        "/C=JP/ST=Tokyo/L=Shibuya/O=Acme/CN=Root CA",
+        template: :root_ca
+      )
+
+    cert =
+      key
+      |> X509.PublicKey.derive()
+      |> X509.Certificate.new(
+        "/C=JP/ST=Tokyo/L=Shibuya/O=Acme/CN=Cert",
+        root_ca,
+        key
+      )
+
+    x5c = [cert |> X509.Certificate.to_der() |> Base.encode64()]
+
+    alg = "RS256"
+    kid = "rs256_202301"
+
+    jwk = %{
+      kid: kid,
+      alg: alg,
+      key: key |> JOSE.JWK.from_key(),
+      x509: JWK.X509.new([x5c: x5c])
+    }
+    |> JWK.new()
+
+    payload = %{"key" => "value"}
+
+    assert {:ok, jws} = JWS.sign(payload, jwk)
+
+    assert {:ok, payload} == JWS.verify(jws, [jwk])
+  end
+
   test "verify" do
     key = File.read!("sample_pem/rsa-2048.pem") |> X509.PrivateKey.from_pem!()
 

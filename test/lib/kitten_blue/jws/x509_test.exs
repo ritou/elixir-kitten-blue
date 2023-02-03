e04c9272a8df887c6cb1acb3000edd9d496792c1
k-asm <asami.kosuke@gmail.com>
2023-02-03 14:21:01 +0900

    refactor: integrate sign and verify jws tests

--- test/lib/kitten_blue/jws/x509_test.exs
+++ test/lib/kitten_blue/jws/x509_test.exs
@@ -3,86 +3,47 @@ defmodule KittenBlue.JWS.X509Test do
 
   alias KittenBlue.{JWK, JWS}
 
-  test "sign" do
-    key = File.read!("sample_pem/rsa-2048.pem") |> X509.PrivateKey.from_pem!()
+  describe "sign and verify" do
+    test "RS256" do
+      key = File.read!("sample_pem/rsa-2048.pem") |> X509.PrivateKey.from_pem!()
 
-    root_ca =
-      X509.Certificate.self_signed(
-        key,
-        "/C=JP/ST=Tokyo/L=Shibuya/O=Acme/CN=Root CA",
-        template: :root_ca
-      )
+      root_ca =
+        X509.Certificate.self_signed(
+          key,
+          "/C=JP/ST=Tokyo/L=Shibuya/O=Acme/CN=Root CA",
+          template: :root_ca
+        )
 
-    cert =
-      key
-      |> X509.PublicKey.derive()
-      |> X509.Certificate.new(
-        "/C=JP/ST=Tokyo/L=Shibuya/O=Acme/CN=Cert",
-        root_ca,
+      cert =
         key
-      )
+        |> X509.PublicKey.derive()
+        |> X509.Certificate.new(
+          "/C=JP/ST=Tokyo/L=Shibuya/O=Acme/CN=Cert",
+          root_ca,
+          key
+        )
 
-    x5c = [cert |> X509.Certificate.to_der() |> Base.encode64()]
+      x5c = [cert |> X509.Certificate.to_der() |> Base.encode64()]
 
-    alg = "RS256"
-    kid = "rs256_202301"
+      alg = "RS256"
+      kid = "rs256_202301"
 
-    jwk = %{
-      kid: kid,
-      alg: alg,
-      key: key |> JOSE.JWK.from_key(),
-      x509: JWK.X509.new([x5c: x5c])
-    }
-    |> JWK.new()
-
-    payload = %{"key" => "value"}
-
-    assert {:ok, jws} = JWS.sign(payload, jwk)
-
-    assert {:ok, payload} == JWS.verify(jws, [jwk])
-  end
-
-  test "verify" do
-    key = File.read!("sample_pem/rsa-2048.pem") |> X509.PrivateKey.from_pem!()
-
-    root_ca =
-      X509.Certificate.self_signed(
-        key,
-        "/C=JP/ST=Tokyo/L=Shibuya/O=Acme/CN=Root CA",
-        template: :root_ca
-      )
-
-    cert =
-      key
-      |> X509.PublicKey.derive()
-      |> X509.Certificate.new(
-        "/C=JP/ST=Tokyo/L=Shibuya/O=Acme/CN=Cert",
-        root_ca,
-        key
-      )
-
-    x5c = [cert |> X509.Certificate.to_der() |> Base.encode64()]
-
-    alg = "RS256"
-    kid = "rs256_202301"
-    payload = %{"key" => "value"}
-
-    jws =
-      key
-      |> JOSE.JWK.from_key()
-      |> JOSE.JWS.sign(
-        payload |> Jason.encode!(),
+      jwk =
         %{
-          "alg" => alg,
-          "kid" => kid,
-          "x5c" => x5c
+          kid: kid,
+          alg: alg,
+          key: key |> JOSE.JWK.from_key(),
+          x509: JWK.X509.new(x5c: x5c)
         }
-      )
-      |> JOSE.JWS.compact()
-      |> elem(1)
+        |> JWK.new()
 
-    jwk = JWK.X509.new_from_jws_token(jws, root_ca)
+      payload = %{"key" => "value"}
 
-    assert {:ok, payload} == JWS.verify(jws, [jwk])
+      assert {:ok, jws} = JWS.sign(payload, jwk)
+
+      assert jwk = JWK.X509.new_from_jws_token(jws, root_ca)
+
+      assert {:ok, payload} == JWS.verify(jws, [jwk])
+    end
   end
 end

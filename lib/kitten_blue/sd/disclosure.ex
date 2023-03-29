3caf90d2f8de96ffb375288b95b51235c493f16e
ritou <ritou.06@gmail.com>
2023-03-29 14:38:32 +0900

    fix logic manually

--- lib/kitten_blue/sd/disclosure.ex
+++ lib/kitten_blue/sd/disclosure.ex
@@ -11,7 +11,7 @@ defmodule KittenBlue.SD.Disclosure do
   https://github.com/authlete/sd-jwt
   """
 
-  defstruct salt: :crypto.strong_rand_bytes(16) |> Base.url_encode64(padding: false),
+  defstruct salt: "",
             claim_name: "",
             claim_value: nil,
             json: "",
@@ -32,11 +32,15 @@ defmodule KittenBlue.SD.Disclosure do
   @doc """
   Constructor with a pair of claim name and claim value. A salt is randomly generated.
   """
-  def new(claim_name, claim_value, salt \\ nil) when is_nil(claim_name) or is_nil(claim_value) do
+  def new(claim_name, claim_value, salt \\ nil)
+
+  def new(claim_name, claim_value, _salt) when is_nil(claim_name) or is_nil(claim_value) do
     raise ArgumentError, "claim_name and claim_value cannot be nil"
   end
 
-  def new(claim_name, claim_value, salt \\ nil) do
+  def new(claim_name, claim_value, salt) do
+    salt = salt || :crypto.strong_rand_bytes(16) |> Base.url_encode64(padding: false)
+
     %__MODULE__{salt: salt, claim_name: claim_name, claim_value: claim_value}
     |> generate_json()
     |> generate_disclosure()
@@ -46,11 +50,11 @@ defmodule KittenBlue.SD.Disclosure do
 
   defp generate_json(%{salt: salt, claim_name: claim_name, claim_value: claim_value} = disclosure) do
     json = [salt, claim_name, claim_value] |> Jason.encode!()
-    Map.update!(disclosure, :json, &json/0)
+    Map.put(disclosure, :json, json)
   end
 
   defp generate_disclosure(%{json: json} = disclosure) do
     disclosure_byte_array = json |> String.to_charlist() |> :unicode.characters_to_binary()
-    Map.update!(disclosure, :disclosure, &disclosure_byte_array/0)
+    Map.put(disclosure, :disclosure, disclosure_byte_array)
   end
 end

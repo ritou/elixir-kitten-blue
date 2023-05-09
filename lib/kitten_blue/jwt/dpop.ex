6344abdf5ab8067df73dafb3a2715d7e77bca4c3
ritou <ritou.06@gmail.com>
2023-05-10 00:12:03 +0900

    add empty file for DPoP

---
+++ lib/kitten_blue/jwt/dpop.ex
@@ -0,0 +1,6 @@
+defmodule KittenBlue.JWT.DPoP do
+  @moduledoc """
+  This is a helper module to generate/validate DPoP Proof JWT.
+  ref. https://www.ietf.org/archive/id/draft-ietf-oauth-dpop-16.html#name-dpop-proof-jwt-syntax
+  """
+end
